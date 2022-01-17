3c4b2f2bbd42fd599197bcfd8b5a2f98155097ed
cel54521 <cel54521@gmail.com>
2022-01-17 22:21:49 +0900

    かっこ位置修正

--- clipboard.js
+++ clipboard.js
@@ -1,60 +1,68 @@
 "use strict";
 
-const clipboard = window.requires.clipboard
+const clipboard = window.requires.clipboard;
 let clipboardHistory = [];      // クリップボードの履歴を古い順に格納
 let clipboardHistoryNo = 0;
 
 // ダブルクリックされた場合、ダブルクリックされた要素の内容をクリップボードに戻す
-function clipboard_area_dblClick(e) {
-    let clipboardText = e.currentTarget.innerHTML
+function clipboard_area_dblClick(e)
+{
+    let clipboardText = e.currentTarget.innerHTML;
     
-    clipboardText = clipboardText.substring(5, clipboardText.length-6)
+    clipboardText = clipboardText.substring(5, clipboardText.length-6);
 
-    clipboardText = clipboardText.replace(/&lt;/g, "<")
-    clipboardText = clipboardText.replace(/&gt;/g, ">")
+    clipboardText = clipboardText.replace(/&lt;/g, "<");
+    clipboardText = clipboardText.replace(/&gt;/g, ">");
 
-    clipboard.writeText( clipboardText )
+    clipboard.writeText( clipboardText );
 }
 
 // クリップボード履歴の内容を<div>タグを付与して表示する画面の更新
-function createClipBoardHistoryElement(pasteText) {
-    const clipboardAreaElement = document.getElementById("clipboard_area")
-    const newClipBoardHistoryElement = document.createElement("div")
+function createClipBoardHistoryElement(pasteText)
+{
+    const clipboardAreaElement = document.getElementById("clipboard_area");
+    const newClipBoardHistoryElement = document.createElement("div");
 
     // HTMLタグ置き換え
-    pasteText = pasteText.replace(/</g,"&lt;")
-    pasteText = pasteText.replace(/>/g,"&gt;")
-    newClipBoardHistoryElement.innerHTML = "<pre>" + pasteText + "</pre>"
-    newClipBoardHistoryElement.className = "clipboard-History-Element"
+    pasteText = pasteText.replace(/</g,"&lt;");
+    pasteText = pasteText.replace(/>/g,"&gt;");
+    newClipBoardHistoryElement.innerHTML = "<pre>" + pasteText + "</pre>";
+    newClipBoardHistoryElement.className = "clipboard-History-Element";
     
     newClipBoardHistoryElement.id = "id" + clipboardHistoryNo;
     clipboardHistoryNo++;
 
-    newClipBoardHistoryElement.addEventListener("dblclick", clipboard_area_dblClick)
-    clipboardAreaElement.prepend(newClipBoardHistoryElement)
+    newClipBoardHistoryElement.addEventListener("dblclick", clipboard_area_dblClick);
+    clipboardAreaElement.prepend(newClipBoardHistoryElement);
 }
 
-function intervalProc(){
+function intervalProc()
+{
 
-    let pasteTextWork = clipboard.readText()
+    let pasteTextWork = clipboard.readText();
     let enablePushHistory = false;
 
     // 空でないことと、前回登録した値と同値でないことを確認してから画面更新、登録をする
-    if( clipboardHistory.length == 0 && !(pasteTextWork === undefined ) ){
-        enablePushHistory = true
-    }else if( clipboardHistory[clipboardHistory.length-1] != pasteTextWork ) {
-        enablePushHistory = true
+    if( clipboardHistory.length == 0 && !(pasteTextWork === undefined ) )
+    {
+        enablePushHistory = true;
+    }
+    else if( clipboardHistory[clipboardHistory.length-1] != pasteTextWork )
+    {
+        enablePushHistory = true;
     }
 
-    if(enablePushHistory){
-        createClipBoardHistoryElement(pasteTextWork)
-        clipboardHistory.push( pasteTextWork )
+    if(enablePushHistory)
+    {
+        createClipBoardHistoryElement(pasteTextWork);
+        clipboardHistory.push( pasteTextWork );
     }
 }
 
-onload = function () {
+onload = function ()
+{
     // 1秒毎に更新するイベントを作成
     let interval;
-    this.clearInterval(interval)
+    this.clearInterval(interval);
     interval = setInterval(intervalProc, 1000);
 };
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 "use strict";
 
-const  {app, Menu, Tray,  BrowserWindow, globalShortcut} = require("electron")
-const path = require("path")
-let tray = null
-let globalWin = null
+const  {app, Menu, Tray,  BrowserWindow, globalShortcut} = require("electron");
+const path = require("path");
+let tray = null;
+let globalWin = null;
 
 // タスクトレイ生成
-function createTray() {
-    let imgFilePath = __dirname + "/icon/icon.png"
+function createTray()
+{
+    let imgFilePath = __dirname + "/icon/icon.png";
     const contextMenu = Menu.buildFromTemplate([
         { 
             label: "終了",
@@ -19,23 +20,25 @@ function createTray() {
     tray.setContextMenu(contextMenu);
 }
 
-function createWindow () {
-    const win = new BrowserWindow({
-        x: 0,
-        y: 0,
-        width: 400,
-        height: 300,
-        transparent: true,
-        resizable: false,
-        frame: false,
-        toolbar: false,
-        webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            contextIsolation: true
+function createWindow ()
+{
+    const win = new BrowserWindow(
+        {
+            x: 0,
+            y: 0,
+            width: 400,
+            height: 300,
+            transparent: true,
+            resizable: false,
+            frame: false,
+            toolbar: false,
+            webPreferences: {
+                preload: path.join(__dirname, "preload.js"),
+                contextIsolation: true
         }
     });
 
-    win.loadFile("index.html")
+    win.loadFile("index.html");
 
     return win;
 }
@@ -44,31 +47,36 @@ app.whenReady().then(() => {
     globalWin = createWindow()
 
     app.on("activate", function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0)
+        {
+            createWindow();
+        } 
       })
 
     try
     {
-        createTray()
+        createTray();
     }
     catch(e)
     {
-        console.log(e)
+        console.log(e);
     }
     
-    globalShortcut.register('Alt+C', function() {
-        globalWin.show()
-        globalWin.focus()   // TODO:globalにしない方法にしたい
+    globalShortcut.register('Alt+C', function()
+    {
+        globalWin.show();
+        globalWin.focus();   // TODO:globalにしない方法にしたい
     })
 })
 
 app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") {
-        globalShortcut.unregisterAll()
-        app.quit()
+    if (process.platform !== "darwin")
+    {
+        globalShortcut.unregisterAll();
+        app.quit();
     }   
 })
 
 app.on("browser-window-blur", function () {
-    globalWin.hide()
+    globalWin.hide();
 })
